3e06aa6e44e29dd1089e1423c25c178ce4082a75
Conquerant2135 <manourakotoarinia@gmail.com>
2026-07-02 15:47:05 +0300

    add : basic controller for showing the map with leaflet

---
+++ src/main/java/com/groupe25/hopital/map/controller/MapController.java
@@ -0,0 +1,14 @@
+package com.groupe25.hopital.map.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapController {
+
+    @GetMapping("/")
+    public String index(){
+        return "map/index";
+    }
+    
+}
