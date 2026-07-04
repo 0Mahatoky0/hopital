8df6f160591de6c689696967e524a3fdacdb8f6c
Conquerant2135 <leconquerant70@gmail.com>
2026-07-04 17:51:01 +0300

    add : shit now working

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,3 +1,7 @@
+let userMarker = null;
+let centerMarker = null;
+let routeLayer = null;
+
 let map = L.map("map", {
   center: [-18.8934916, 47.5584519],
   zoom: 10,
@@ -41,3 +45,78 @@ fetch("http://localhost:8080/api/healthCenter/all")
       },
     }).addTo(map);
   });
+
+const locateControl = L.control({ position: "bottomleft" });
+
+locateControl.onAdd = function (map) {
+  const btn = L.DomUtil.create("button", "my-location-btn");
+
+  btn.innerHTML = "Locate";
+  btn.title = "Locate Me";
+
+  L.DomEvent.disableClickPropagation(btn);
+
+  btn.onclick = function () {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+
+      L.marker([lat, lon], { title: "Votre position" }).addTo(map);
+      map.setView([lat, lon], 16);
+
+      getNearestHealthCenter(lat, lon);
+    });
+  };
+
+  return btn;
+};
+
+locateControl.addTo(map);
+
+function getNearestHealthCenter(lat, lon) {
+  fetch("http://localhost:8080/api/healthCenter/nearest", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ lat, lon }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+
+      if (centerMarker) map.removeLayer(centerMarker);
+      if (routeLayer) map.removeLayer(routeLayer);
+
+      const centerCoords = data.center.geometry.coordinates;
+      const centerLat = centerCoords[1];
+      const centerLon = centerCoords[0];
+
+      centerMarker = L.marker([centerLat, centerLon], {
+        icon: L.icon({
+          iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+          iconSize: [30, 30],
+        }),
+      })
+        .addTo(map)
+        .bindPopup(
+          `
+          <b>${data.center.name}</b><br>
+           Centre de sante
+        `,
+        )
+        .openPopup();
+
+      if (data.route && data.route.geometry) {
+        routeLayer = L.geoJSON(data.route.geometry, {
+          style: {
+            color: "#1a73e8",
+            weight: 5,
+            opacity: 0.9,
+          },
+        }).addTo(map);
+      }
+      showInfoPanel(data);
+    })
+    .catch((err) => console.error(err));
+}
