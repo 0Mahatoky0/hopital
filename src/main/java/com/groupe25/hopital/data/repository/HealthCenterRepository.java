ad8ee2816e5488ca37da79b73a7e6c217cf0deef
Conquerant2135 <leconquerant70@gmail.com>
2026-07-04 12:27:09 +0300

    fix : removing getter , setters and no args constructor for using lombok instead

--- src/main/java/com/groupe25/hopital/data/repository/HealthCenterRepository.java
+++ src/main/java/com/groupe25/hopital/data/repository/HealthCenterRepository.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import com.groupe25.hopital.data.model.HealthCenterModel;
 
-@Repository
 public interface HealthCenterRepository extends JpaRepository<HealthCenterModel,Long> {
     @Query("SELECT DISTINCT h.amenity FROM HealthCenterModel h")
     List<String> findDistinctAmenity();
